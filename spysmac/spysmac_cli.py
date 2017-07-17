e129c543ecf9edc893d26cacd184c77ce9435d73
Joshua Marben <joshua.marben@neptun.uni-freiburg.de>
2017-07-17 15:49:22 +0200

    ADD support for multiple folder input

--- spysmac/spysmac_cli.py
+++ spysmac/spysmac_cli.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import logging
 import os
+import shutil
 
 from spysmac.analyzer import Analyzer
 from spysmac.html.html_builder import HTMLBuilder
@@ -31,13 +32,20 @@ class SpySMACCLI(object):
         """
         parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
         req_opts = parser.add_argument_group("Required Options")
-        req_opts.add_argument("--folder", required=True,
-                              help="path to SMAC output")
+        req_opts.add_argument("--folders", required=True, nargs='+',
+                              help="path(s) to SMAC output-directory/ies, "
+                                   "containing at least a runhistory and "
+                                   "a trajectory.")
 
         opt_opts = parser.add_argument_group("Optional Options")
         opt_opts.add_argument("--verbose_level", default=logging.INFO,
                               choices=["INFO", "DEBUG"],
                               help="verbose level")
+        opt_opts.add_argument("--output", default="",
+                              help="Path to folder in which to save the combined HTML-report. "
+                                   "If not specified, spySMAC will only generate "
+                                   "the individual reports in the corresponding "
+                                   "output-folders.")
         args_, misc = parser.parse_known_args()
 
         if args_.verbose_level == "INFO":
@@ -46,33 +54,65 @@ class SpySMACCLI(object):
             logging.basicConfig(level=logging.DEBUG)
 
         # SMAC results
-        folder = args_.folder
+        folders = args_.folders
+        websites = dict()
 
-        # Create Scenario (disabling output_dir to avoid cluttering)
-        in_reader = InputReader()
-        scen = in_reader.read_scenario_file(os.path.join(folder, 'scenario.txt'))
-        scen['output_dir'] = ""
-        scen = Scenario(scen)
+        for folder in folders:
+            # Create Scenario (disable output_dir to avoid cluttering)
+            in_reader = InputReader()
+            scen = in_reader.read_scenario_file(os.path.join(folder, 'scenario.txt'))
+            scen['output_dir'] = ""
+            scen = Scenario(scen)
 
-        # Load runhistory and trajectory
-        rh = RunHistory(average_cost)
-        rh.load_json(os.path.join(folder, 'runhistory.json'), scen.cs)
-        traj_fn = os.path.join(folder, "traj_aclib2.json")
-        trajectory = TrajLogger.read_traj_aclib_format(fn=traj_fn, cs=scen.cs)
+            # Load runhistory and trajectory
+            rh = RunHistory(average_cost)
+            rh.load_json(os.path.join(folder, 'runhistory.json'), scen.cs)
+            traj_fn = os.path.join(folder, "traj_aclib2.json")
+            trajectory = TrajLogger.read_traj_aclib_format(fn=traj_fn, cs=scen.cs)
 
-        # Create SpySMAC-folder
-        output = os.path.join(folder, 'SpySMAC')
-        if not os.path.exists(output):
-            os.makedirs(output)
+            # Create SpySMAC-folder
+            output = os.path.join(folder, 'SpySMAC')
+            if not os.path.exists(output):
+                os.makedirs(output)
 
-        # Impute missing data via validation
-        new_rh_path = os.path.join(output, 'validated_rh.json')
-        validator = Validator(scen, trajectory, new_rh_path) # args_.seed)
-        new_rh = validator.validate('def+inc', 'train+test', 1, -1, rh, None)
+            # Impute missing data via validation
+            new_rh_path = os.path.join(output, 'validated_rh.json')
+            validator = Validator(scen, trajectory, new_rh_path) # args_.seed)
+            new_rh = validator.validate('def+inc', 'train+test', 1, -1, rh, None)
 
-        # Analyze and build HTML
-        analyzer = Analyzer(scen, new_rh, trajectory[-1]['incumbent'],
-                            output=output)
-        analyzer.analyze()
-        analyzer.build_html()
+            # Analyze and build HTML
+            analyzer = Analyzer(scen, new_rh, trajectory[-1]['incumbent'],
+                                output=output)
+            analyzer.analyze()
+            websites[folder] = analyzer.build_html()
 
+        if args_.output:
+            # Combined website
+            if not os.path.exists(args_.output):
+                os.makedirs(args_.output)
+
+            # Fix figures (copy them to combined output-path and fix paths)
+            def fix_dict(d, parent):
+                """
+                Recursively fix figures (copy them to combined output-path
+                and fix paths). Parent is used to determine the subfolder-names.
+                """
+                new_dict = {}
+                for k, v in d.items():
+                    if isinstance(v, dict):
+                        parent = parent if parent else k  # Only insert scenario-names
+                        new_dict[k] = fix_dict(v, parent)
+                    elif k == 'figure':
+                        new_path = os.path.join(args_.output, v)
+                        if not os.path.exists(os.path.splitext(new_path)[0]):
+                            os.makedirs(os.path.splitext(new_path)[0])
+                        shutil.copy(v, new_path)
+                        new_dict[k] = new_path
+                    else:
+                        new_dict[k] = v
+                return new_dict
+
+            websites = fix_dict(websites, "")
+
+            html_builder = HTMLBuilder(args_.output, "SpySMAC report")
+            html_builder.generate_html(websites)
