f0c262c5046077ed0a6a76d69843acaba729b760
Joshua Marben <joshua.marben@neptun.uni-freiburg.de>
2017-09-15 11:29:38 +0200

    RM shadow around tables and add branin to tests

---
+++ test/test_files/branin/branin.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def branin(x, inst=None, inst_specs=None):
+    x1 = x[0]
+    x2 = x[1]
+    a = 1.
+    b = 5.1 / (4.*np.pi**2)
+    if inst:
+        a = a * float(inst)
+    if inst_specs:
+        b = b / float(inst_specs)
+    c = 5. / np.pi
+    r = 6.
+    s = 10.
+    t = 1. / (8.*np.pi)
+    ret = a*(x2-b*x1**2+c*x1-r)**2+s*(1-t)*np.cos(x1)+s
+    return ret
