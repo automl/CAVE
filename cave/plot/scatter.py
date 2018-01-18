02da970bfab0cf5a6371ff9b43a10ef2d3aff50f
shukon <jo.ma@posteo.de>
2018-01-18 20:02:13 +0100

    ADD split scatter in two figures, MAINT fix scatter ticks

--- cave/plot/scatter.py
+++ cave/plot/scatter.py
@@ -102,10 +102,6 @@ def plot_scatter_plot(x_data, y_data, labels, title="", debug=False,
                                  ])
 
     # set initial limits
-    x_min = min([min(x) for x in x_data])
-    y_min = min([min(y) for y in y_data])
-    x_max = max([max(x) for x in x_data])
-    y_max = max([max(y) for y in y_data])
     if min_val is not None:
         auto_min_val = min([x_min, y_min, min_val])
     else:
@@ -330,20 +326,6 @@ def plot_scatter_plot(x_data, y_data, labels, title="", debug=False,
                         new_ticks_label.append(str(r"$10^{%d}$" %
                                                    int(np.log10(ticks_x[l_idx]))))
             ax.set_xticklabels(new_ticks_label)  # , rotation=45)
-
-            ticks_y = ax.get_yticks()
-            new_ticks_label = list()
-            for l_idx in range(len(ticks_y)):
-                if ticks_y[l_idx] < maximum_value:
-                    if 0 < ticks_y[l_idx] < 1:
-                        new_ticks_label.append(str(r"$10^{%d}$" %
-                                                   int(np.log10(ticks_y[l_idx]))))
-                    if 1 <= ticks_y[l_idx] < 1000:
-                        new_ticks_label.append(str(r"$%d^{ }$" %
-                                                   int(ticks_y[l_idx])))
-                    if 1000 <= ticks_y[l_idx]:
-                        new_ticks_label.append(str(r"$10^{%d}$" %
-                                                   int(np.log10(ticks_y[l_idx]))))
             ax.set_yticklabels(new_ticks_label)  # , rotation=45)
 
     # Change fontsize for ticklabels
