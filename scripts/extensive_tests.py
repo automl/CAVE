a914e6420edcd14d194f613ec107069a879ef942
shuki <shukon@users.noreply.github.com>
2019-01-21 18:59:24 +0100

    ADD bohb to extended tests

--- scripts/extensive_tests.py
+++ scripts/extensive_tests.py
@@ -100,6 +100,49 @@ def get_scenarios():
             })
     return scens
 
+def generate_bohb_data():
+    import warnings
+    import hpbandster.core.nameserver as hpns
+    import hpbandster.core.result as hpres
+    from hpbandster.optimizers import BOHB as BOHB
+
+    run_id = '0'  # Every run has to have a unique (at runtime) id.
+    NS = hpns.NameServer(run_id=run_id, host='localhost', port=0)
+    ns_host, ns_port = NS.start()
+
+    from neural_opt import MyWorker, get_configspace
+
+    w = MyWorker(nameserver=ns_host,
+             nameserver_port=ns_port,
+             run_id=run_id,  # same as nameserver's
+            )
+    w.run(background=True)
+
+    # Log the optimization results for later analysis
+    result_logger = hpres.json_result_logger(directory='test/general_example/results/bohb_full_configspace',
+                                             overwrite=True)
+
+    bohb = BOHB(configspace=get_configspace(),
+                run_id=run_id,  # same as nameserver's
+                eta=2, min_budget=5, max_budget=100,  # Hyperband parameters
+                nameserver=ns_host, nameserver_port=ns_port,
+                result_logger=result_logger,
+                )
+
+    # Then start the optimizer. The n_iterations parameter specifies
+    # the number of iterations to be performed in this run
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        res = bohb.run(n_iterations=2)
+
+    # After the run is finished, the services started above need to be shutdown.
+    # This ensures that the worker, the nameserver and the master all properly exit
+    # and no (daemon) threads keep running afterwards.
+    # In particular we shutdown the optimizer (which shuts down all workers) and the nameserver.
+    bohb.shutdown(shutdown_workers=True)
+    NS.shutdown()
+
+
 def print_help():
     print("This script will generate scenarios for the corner cases of provided information (with/without features, instances, etc.) using toy data.\n"
           "Start this script with one of the following arguments in a suitable python-environment (that fulfills CAVE's requirements):\n"
@@ -115,6 +158,7 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print_help()
     elif sys.argv[1] == 'generate':
+        generate_bohb_data()
         for scen in get_scenarios():
             scenario = Scenario(scen)
             smac = SMAC(scenario=scenario, rng=np.random.RandomState(42))
