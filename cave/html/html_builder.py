527f3f4822bd5e2a56cedbbbb1da7fa7c3df8f73
shuki <shukon@users.noreply.github.com>
2019-01-29 21:40:46 +0100

    MAINT html-tables scrollable on overflow

--- cave/html/html_builder.py
+++ cave/html/html_builder.py
@@ -255,7 +255,7 @@ for (i = 0; i < acc.length; i++) {
             elif k == "figure_x2":
                 div += figure_to_html(v, prefix=self.output_dn, max_in_a_row=2)
             elif k == "table":
-                div += "<div align=\"center\">\n{}\n</div>\n".format(v)
+                div += "<div style=\"overflow-x: auto\" align=\"center\">\n{}\n</div>\n".format(v)
             elif k == "html":
                 div += ("<div align=\"center\">\n<a href='{}'>Interactive "
                         "Plot</a>\n</div>\n".format(v[len(self.output_dn):].lstrip("/")))
