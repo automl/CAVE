a3cdd30f436688addc1671d0909466d0f7f1139f
shukon <jo.ma@posteo.de>
2019-10-25 22:55:34 +0200

    FIX travis build maybe by checking if we even want to build the docs...

--- doc/conf.py
+++ doc/conf.py
@@ -16,12 +16,19 @@ import datetime
 import sys
 import os
 import shlex
-import sphinx_rtd_theme
 import matplotlib
 matplotlib.use('agg')
 
 sys.path.insert(0, '..')
 import cave
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+else:
+    html_theme = 'default'
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
